4505624cd375d378012b796f45ae8d63e8af40e0
Kevin <imbesci@gmail.com>
2022-03-22 17:23:46 -0400

    Updated create article page

--- substack/articles/forms.py
+++ substack/articles/forms.py
@@ -1,13 +1,12 @@
-from csv import list_dialects
 from django import forms
 from . import models
 
 
 #add form for making new articles
 class CreateArticle(forms.ModelForm):
-    title = forms.CharField(max_length = 200, widget = forms.Textarea(attrs = {'placeholder': 'Choose a unique title for your work', 'min_length':1, 'size': 50}))
-    description = forms.CharField(widget = forms.Textarea(attrs = {'placeholder': 'Write a brief descrption...', 'rows':'3', 'cols': '70'}))
-    # body = forms.CharField(widget = forms.Textarea(attrs = {'placeholder': 'Write the body of your article...', 'rows':'10', 'cols': '100'},))
+    title = forms.CharField(max_length = 200, widget = forms.Textarea(attrs = {'placeholder': 'Enter title...', 'min_length':1, 'rows':1}))
+    description = forms.CharField(widget = forms.Textarea(attrs = {'placeholder': 'Write a brief descrption...', 'rows':'2', }))
+    
 
     class Meta:
         model = models.Article
