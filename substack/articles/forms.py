efc8cde1730eec54e4f57018c54b6fb1bd4cadab
Kevin <imbesci@gmail.com>
2022-03-22 18:48:48 -0400

    More changes

--- substack/articles/forms.py
+++ substack/articles/forms.py
@@ -5,8 +5,7 @@ from . import models
 #add form for making new articles
 class CreateArticle(forms.ModelForm):
     title = forms.CharField(max_length = 200, widget = forms.Textarea(attrs = {'placeholder': 'Enter title...', 'min_length':1, 'rows':1}))
-    description = forms.CharField(widget = forms.Textarea(attrs = {'placeholder': 'Write a brief descrption...', 'rows':'2', }))
-    
+    description = forms.CharField(widget = forms.Textarea(attrs = {'placeholder': 'Write a brief description...', 'rows':'2', }))
 
     class Meta:
         model = models.Article
